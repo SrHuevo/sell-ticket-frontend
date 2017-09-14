f78a01023bddd847ff69a5ac1686f5d5c9467e12
daniel garoz solis <daniel garoz solis>
2017-09-14 20:34:36 +0200

    add angularcli dependency and node engine

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      )
-    }
-    next()
-  }
-}
-app.use(forceSSL())
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//         ['https://', req.get('Host'), req.url].join('')
+//       )
+//     }
+//     next()
+//   }
+// }
+// app.use(forceSSL())
 app.use(express.static(__dirname + '/dist'))
 
 app.get('/*', function(req, res) {
